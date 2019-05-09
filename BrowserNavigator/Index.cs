e0130b9516bb88b1d1a04fe608e3de9369266010
Snyp <36605990+gabruw@users.noreply.github.com>
2019-05-08 22:40:18 -0300

    All

---
+++ BrowserNavigator/Index.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navegator
+{
+    public class Index
+    {
+        static void Main(string[] args)
+        {
+            var driverService = InternetExplorerDriverService.CreateDefaultService();
+            driverService.LibraryExtractionPath = Environment.CurrentDirectory;
+
+            var options = new InternetExplorerOptions();
+            options.EnableNativeEvents = true;
+            options.IgnoreZoomLevel = true;
+            options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+
+            IWebDriver ieDriver = new InternetExplorerDriver(driverService, options, TimeSpan.FromSeconds(180));
+            ieDriver.Navigate().GoToUrl("http://accounts.atp.com/Login/Login?spId=aviationHub");
+
+            var html = ieDriver.PageSource;
+
+            ieDriver.Quit();
+        }
+    }
+}

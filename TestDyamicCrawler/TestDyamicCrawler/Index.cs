ed8b9c9d5407590c98a1406137a23b30fb20c392
Snyp <gabrielmarques.mg@gmail.com>
2019-05-10 12:31:10 -0300

    np

---
+++ TestDyamicCrawler/TestDyamicCrawler/Index.cs
@@ -0,0 +1,41 @@
+﻿using mshtml;
+using SHDocVw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDyamicCrawler
+{
+    class Index
+    {
+        static void Main(string[] args)
+        {
+            Index newIndex = new Index();
+
+            var url = "http://www.google.com.br";
+            newIndex.BrowserRequestGET(url);
+
+            Console.ReadKey();
+        }
+
+        public string BrowserRequestGET(string url)
+        {
+            Explorer explorer = new Explorer();
+
+            var html = string.Empty;
+
+            explorer.NavigateCompleteEvent += new Explorer.NavigateCompleteEventHandler(GetHtml);
+
+            explorer.Navigate(url);
+
+            return html;
+        }
+
+        static void GetHtml(HTMLDocument document, string url)
+        {
+            var html = document.documentElement.innerHTML;
+        }
+    }
+}
